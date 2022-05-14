07c277aaf546d13a35a98100f0c24f5ce2c04111
Sandeep Jha <91358961+ft-jsandeep@users.noreply.github.com>
2022-05-14 20:44:04 +0530

    Added basic styling to the CRUD application

--- seeds/index.js
+++ seeds/index.js
@@ -17,10 +17,15 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 5000) + 1000;
 
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://placeimg.com/640/480/nature",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati libero nam culpa in fugiat amet debitis voluptatem at perspiciatis soluta! Repellat assumenda consequatur perspiciatis quas temporibus pariatur eos obcaecati soluta!",
+      price,
     });
 
     await camp.save();
